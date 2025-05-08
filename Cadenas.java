dbd4395d1bbf8c0dedb436e77ba84fa31ff48bb3
aliiyjm <20243ds130@utez.edu.mx>
2025-05-08 09:40:09 -0600

    Se imprimio el nombre como se pidio

--- Cadenas.java
+++ Cadenas.java
@@ -2,9 +2,12 @@ import java.util.Scanner;
 
 public class Cadenas {
     public static void main(String[] args){
-        String nombre;
+        //String nombre;
         String entrada = new Scanner(System.in);
         System.out pintln("Escibe tu nombre");
         String nombre = entrada.nextLine();
+        System.out println("Escribe tu apellido");
+        String apellido = entrada.nextLine();
+        System.out pintln("Tu nombre es: "+ nombre + apellido);
     }
 }
