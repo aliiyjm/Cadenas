1b7c6f2c7674cbd43c543b74beaf27b95fc92cab
aliiyjm <20243ds130@utez.edu.mx>
2025-05-08 09:34:07 -0600

    Se configuro la salida de texto

--- Cadenas.java
+++ Cadenas.java
@@ -4,5 +4,7 @@ public class Cadenas {
     public static void main(String[] args){
         String nombre;
         String entrada = new Scanner(System.in);
+        System.out pintln("Escibe tu nombre");
+        String nombre = entrada.nextLine();
     }
 }
